b34dc91d5d28e4201a483dcb19d18d2d162bd428
Joe Vacovsky Jr <vacovsky@gmail.com>
2016-05-05 23:30:17 -0700

    D

--- config.py
+++ config.py
@@ -2,3 +2,11 @@ REDDIS_CONN = "192.168.111.50"
 PUBSUB_NAME = 'BlinkBlock'
 LOG_LOCATION = "pihole.log"
 GRAVITY_LIST = "gravity.list"
+
+"""
+17 = green, 22 = blue, 27 = red
+"""
+
+GREEN = 17
+BLUE = 22
+RED = 27
