7e8956c4db372224eb28a3b638628b3f0d77a79b
Joe Vacovsky Jr <vacovsky@gmail.com>
2016-05-05 23:28:18 -0700

    Made pin a config value

--- BlinkControl.py
+++ BlinkControl.py
@@ -1,5 +1,6 @@
 from RPi import GPIO
 import time
+from config import RED, BLUE, GREEN
 
 
 class BlinkControl:
@@ -7,9 +8,9 @@ class BlinkControl:
     def __init__(self):
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(17, GPIO.OUT)
-        GPIO.setup(27, GPIO.OUT)
-        GPIO.setup(22, GPIO.OUT)
+        GPIO.setup(GREEN, GPIO.OUT)
+        GPIO.setup(RED, GPIO.OUT)
+        GPIO.setup(BLUE, GPIO.OUT)
 
     def blink(self, base_color="green", flash_color="red", count=1, interval=0.05):
         blinked = 0
@@ -51,67 +52,62 @@ class BlinkControl:
                 self.blink_yellow(interval)
             elif flash_color == "off":
                 self.blink_off(interval)
-
             blinked += 1
             self.blink_off()
 
-    """
-    17 = green, 22 = blue, 27 = red
-    """
-
     def blink_blue(self, interval=0.05):
-        GPIO.output(17, 0)
-        GPIO.output(27, 0)
-        GPIO.output(22, 1)
+        GPIO.output(GREEN, 0)
+        GPIO.output(RED, 0)
+        GPIO.output(BLUE, 1)
         time.sleep(interval)
         self.blink_off()
 
     def blink_red(self, interval=0.05):
-        GPIO.output(17, 0)
-        GPIO.output(27, 1)
-        GPIO.output(22, 0)
+        GPIO.output(GREEN, 0)
+        GPIO.output(RED, 1)
+        GPIO.output(BLUE, 0)
         time.sleep(interval)
         self.blink_off()
 
     def blink_yellow(self, interval=0.05):
-        GPIO.output(17, 1)
-        GPIO.output(27, 1)
-        GPIO.output(22, 0)
+        GPIO.output(GREEN, 1)
+        GPIO.output(RED, 1)
+        GPIO.output(BLUE, 0)
         time.sleep(interval)
         self.blink_off()
 
     def blink_green(self, interval=0.05):
-        GPIO.output(17, 1)
-        GPIO.output(27, 0)
-        GPIO.output(22, 0)
+        GPIO.output(GREEN, 1)
+        GPIO.output(RED, 0)
+        GPIO.output(BLUE, 0)
         time.sleep(interval)
         self.blink_off()
 
     def blink_white(self, interval=0.05):
-        GPIO.output(17, 1)
-        GPIO.output(27, 1)
-        GPIO.output(22, 1)
+        GPIO.output(GREEN, 1)
+        GPIO.output(RED, 1)
+        GPIO.output(BLUE, 1)
         time.sleep(interval)
         self.blink_off()
 
     def blink_purple(self, interval=0.05):
-        GPIO.output(17, 0)
-        GPIO.output(27, 1)
-        GPIO.output(22, 1)
+        GPIO.output(GREEN, 0)
+        GPIO.output(RED, 1)
+        GPIO.output(BLUE, 1)
         time.sleep(interval)
         self.blink_off()
 
     def blink_teal(self, interval=0.05):
-        GPIO.output(17, 1)
-        GPIO.output(27, 0)
-        GPIO.output(22, 1)
+        GPIO.output(GREEN, 1)
+        GPIO.output(RED, 0)
+        GPIO.output(BLUE, 1)
         time.sleep(interval)
         self.blink_off()
 
     def blink_off(self, interval=0):
-        GPIO.output(17, 0)
-        GPIO.output(27, 0)
-        GPIO.output(22, 0)
+        GPIO.output(GREEN, 0)
+        GPIO.output(RED, 0)
+        GPIO.output(BLUE, 0)
         time.sleep(interval)
 
 
